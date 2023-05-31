b28af80efe105d23e445d09fc8b7bd608fc8e2d6
namevug <ciavug@gmail.com>
2023-05-31 17:40:56 -0300

    feat: layout page in products

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import '../globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,7 +16,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <main className='flex main-h-screen flex-col items-center justify-between p-24'>
+    <h1 className='text-2xl font-semibold'>Página de produtos</h1>
+
+    <hr className="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" /> 
+    <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900" />
+
+    <Link href='/'>
+    <p>Home</p>
+    </Link>
+    <br />
+    {children}
+    </main>
+    </body>
     </html>
   )
 }
