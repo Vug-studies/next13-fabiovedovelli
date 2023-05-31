88ceff3d23f2798faaaf40dbf6ac745c9df2c963
namevug <ciavug@gmail.com>
2023-05-31 17:38:59 -0300

    feat: loading/error page

---
+++ src/app/products/error.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export default function Error() {
+  return <p className="text-center font-semibold text-2xl text-red-600">Erro na aplicação!</p>
+}
---
+++ src/app/products/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <p className="text-center font-semibold text-2xl text-blue-700">Loading...</p>
+}
